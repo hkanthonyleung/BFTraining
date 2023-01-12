4b391a368af83d436a81e579e9b29c397c5b2120
unknown <anthony.leung@biz-forest.com>
2023-01-12 11:39:58 +0800

    [Add] exday402

---
+++ exerciseday301/models/sale_order.py
@@ -0,0 +1,23 @@
+
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    
+    total_item = fields.Integer(string="Total Items", compute='_compute_count_total')
+    @api.depends('order_line.product_uom_qty')
+    def _compute_count_total(self):
+        for rec in self:
+            rec.total_item = sum(rec.order_line.mapped('product_uom_qty'))
+    #By Loop
+    # def _compute_count_total(self):
+    #     for rec in self:
+    #         count_item = 0                
+    #         for order_line in rec.order_line:
+    #           count_item = count_item + order_line.product_uom_qty  
+    #         rec.total_item = count_item
+
+    phone = fields.Char(string="Phone", related="partner_id.phone")
+    mobile = fields.Char(string="Mobile", related="partner_id.mobile")
