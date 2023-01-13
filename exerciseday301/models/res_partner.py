06831e03d37e34a69ff1ffd06e51ec9fb02f86cb
unknown <anthony.leung@biz-forest.com>
2023-01-13 11:34:23 +0800

    [Add] ex day 5 02

--- exerciseday301/models/res_partner.py
+++ exerciseday301/models/res_partner.py
@@ -5,3 +5,11 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'   
     
     date_of_birth = fields.Date(string="Date of Birth")
+    @api.onchange('name')
+    def onchange_name(self):
+        for rec in self:
+            if self.name:  
+                rec.email = rec.name.replace(' ', '_').lower() + '@odoo.com'        
+    
+    signature = fields.Binary(string="Signature")
+    
